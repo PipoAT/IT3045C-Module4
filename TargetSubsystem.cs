50b6844566b5830bc5afcd5c60d18ec30fd4433d
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-02-06 16:00:13 -0500

    Added shell/outline files per assignment

--- TargetSubsystem.cs
+++ TargetSubsystem.cs
@@ -2,5 +2,7 @@ namespace RobotObjectModel;
 
 public class TargetSubsystem
 {
-    
+    public Sonar SonarOne { get; set; } = new();
+
+    public Camera CameraOne { get; set; } = new();
 }
--- TurretSubsystem.cs
+++ TurretSubsystem.cs
@@ -2,5 +2,7 @@ namespace RobotObjectModel;
 
 public class TurretSubsystem
 {
-    
+    public Servos ServosOne { get; set; } = new();
+
+    public Shooter ShooterOne { get; set; } = new();
 }
